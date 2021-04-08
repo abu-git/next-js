9ffd6c50f4d3fd2708b509af38bb9ac83aa23dec
Samuel Abu <abu.sammie@gmail.com>
2021-04-08 17:23:51 +0200

    getStaticProps used for data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
@@ -9,7 +9,19 @@ export default function Home() {
         <meta name='keywords' content='web development,
           programming' />
       </Head>
-      <h1>Welcome to Next js</h1>
+
+      {articles.map(article => <h3 key={article.id}>{article.title}</h3>)} 
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
