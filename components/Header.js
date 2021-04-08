39344c6b8a6fd5d3d6f589682bbd305321bc04da
Samuel Abu <abu.sammie@gmail.com>
2021-04-08 12:04:33 +0200

    styled jsx explained

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,17 @@ import headerStyles from '../styles/Header.module.css'
 const Header = () => {
     return (
         <div>
-           <h1><span>WebDev</span> News</h1> 
+            <h1 className='title'>
+                <span>WebDev</span> News
+            </h1>
+
+            <style jsx>
+                {`
+                    .title {
+                        color: red
+                    }
+                `}   
+            </style> 
         </div>
     )
 }
