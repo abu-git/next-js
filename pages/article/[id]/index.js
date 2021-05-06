e3b13d9e104ec16fa8dde6cacd3634d88c863040
Samuel Abu <abu.sammie@gmail.com>
2021-05-06 22:32:27 +0200

    api routes usage example

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import {server} from '../../../config'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -15,6 +16,34 @@ const article = ({article}) => {
     )
 }
 
+export const getStaticProps = async (context) => {
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`${server}/api/articles`)
+
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+
+    const paths = ids.map(id => ({params: {id: id.toString()}}))
+    
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+/*
 export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
 
@@ -40,6 +69,6 @@ export const getStaticPaths = async () => {
         paths,
         fallback: false,
     }
-}
+}*/
 
 export default article
